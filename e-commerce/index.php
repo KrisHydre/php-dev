678223e39cae6e39743ef5ff25216e360a7636ef
KrisHydre <45457995+KrisHydre@users.noreply.github.com>
2020-12-03 09:05:02 +0700

    Add files via upload

---
+++ e-commerce/index.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html>
+<title> Welcome </title>
+<link rel="stylesheet" href="css/style.css"/>
+<head>
+
+
+<img  width="400" style="display: block; margin-left: auto; margin-right: auto;" src="images/homepage.png" >
+	
+
+<h1 style="color:black;text-align:center;"> Welcome to Post Manager Tools </h1>
+</head>
+
+<body>
+  <!-- Main container start here -->
+   <div class="w3-bar">
+
+	<tr>
+	<h2 style="color:red;">
+		If this is the first time you run the tools: </h2>
+	<a href="./admin/includes/database.php" class="w3-btn w3-black">
+	  <button class="w3-btn w3-black" style="float:center;">FIRST RUN </button></a>
+	  
+	<h2 style="color:Green;">
+		For Users Roles: </h2>
+	  <a href="./customer/show.php">
+	  <button >USER</button></a>
+	  
+	<h2 style="color:blue;">
+		For Admin Roles: </h2>
+	  <a href="./admin/includes/admin-show.php">
+	  <button >ADMIN</button></a>
+
+	</div> 
+	<div class="content_wrapper">
+	</div>	<!--content wrapper-->
+<!--End Main container -->
+</body>
+</html>
+
