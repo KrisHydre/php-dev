8b2e9f7b80c31d278958aabaf271230ff147c910
KrisHydre <45457995+KrisHydre@users.noreply.github.com>
2020-12-03 16:07:55 +0700

    Add files via upload

--- mvc/customer/show.php
+++ mvc/customer/show.php
@@ -11,42 +11,63 @@ include "../admin/includes/database.php" ;
 	$i = 1 ;
 ?>
 <head>
- <h1 size=40 > <b> No Header </h1>
+ <h1 size=40 > <b> Show Post </h1>
 </head>
 
 <body>
 <!-- Main container start here -->
-		<table>
-		  <tr id="post-tables">
+
+		<?php
+			# extract the date from mysql
+		if (isset ($_GET ['id'])) {
+			$script = "SELECT title, image, descriptions FROM post_manager WHERE id=\"" . $_GET['id'] . "\"" ;
+			echo $script ; 
+			$use = $con->query ("USE posts_manager") ;
+			$run = $con->query($script) ;
+			$row = $run->fetch_assoc() ;
+			$title = $row['title'] ;
+			$img = "../" . $row['image'] ;
+			$des = $row['descriptions'] ;
+			echo "	<!doctype html>
+				<html>
+				<title> Posts Listing </title>
+				<head>
+				<h1 size=40 > <b>  $title  </h1> <br>
+				</head>
+				<body>
+				<!-- Main container start here -->
+				  	<img id=\"logo\" width='600px' src=' " . $img ."' /><br>
+					<br><p>  " . $des ."  </p>	
+				<!--End Main container -->
+				</body>";
+			} 
+		else {
+		echo "<table>
+		  <tr id=\"post-tables\">
 		    <th> ID </th>
 		    <th> Thumb </th>
 		    <th> Title </th>
-		  </tr>
-		<?php
-			/*if($con !== false) {
-				echo "Connect Successfully" ;
-			} else {
-				echo "It just fucking doesn't work!";
-			}*/
+		  </tr>" ;
+		while ($row = $run->fetch_assoc()) {
+
+			echo "<tr id=".$i.">" ;
+			echo ("<td>".
+			"<a href=\"show.php?id=". $row["id"] . "\">" . $row["id"] .
+			"</a>"."</td>".
 			
-			# extract the date from mysql
-			while ($row = $run->fetch_assoc()) {
-				echo "<tr id=".$i.">" ;
-				echo ("<td>".
-				"<a href=\"/mvc/postlist/p" . $i . ".php\">". $row["id"] . 
-				"</a>"."</td>".
-				
-				"<td>".
-				"<a href=\"/mvc/postlist/p" . $i . ".php\">" .
-				"<img id=\"p" . $i . "\"" . "src=../" . $row["image"] . " />" . 
-				"</a>"."</td>".
-				
-				"<td>".
-				"<a href=\"/mvc/postlist/p" . $i . ".php\">" . $row["title"] .
-				"</a>"."</td>");
-				echo "</tr>" ;
-				$i++ ;
+			"<td>".
+			"<a href=\"show.php?id=" . $row["id"] . "\">" .
+			"<img id=\"p" . $i . "\"" . "src=../" . $row["image"] . " />" . 
+			"</a>"."</td>".
+			
+			"<td>".
+			"<a href=\"show.php?id=" . $row["id"] . "\">"  . $row["title"] .
+			"</a>"."</td>");
+			echo "</tr>" ;
+			$i++ ;
+
 			}
+		}
 			$con->close() ;
 		?>
 		</table>
